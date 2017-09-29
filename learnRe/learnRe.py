0702ae87f00c9e9631fa64ca2504e06e66152f78
Unknown <hdhq_smile@buaa.edu.cn>
2017-09-29 13:17:47 +0800

    合并代码

--- learnRe/learnRe.py
+++ learnRe/learnRe.py
@@ -5,7 +5,7 @@ if match:
 # 匹配结果要用if检测是否为空
 # re函数 search match (从头位置开始) find_all(返回列表类型)
 # re.split 将字符串按照正则表达式匹配结果进行分割
-# re.finditer 迭代获得匹配的结果
+# re.finditer 迭代获得匹配的结果 ,返回match对象的列表
 
 for m in re.finditer(r'[1-9]\d{5}', 'BIU 100081,TSU 100084'):
     if m:
@@ -20,7 +20,7 @@ print(splitStr)
 splitStr = re.split(r'[1-9]\d{5}', 'BIU 100081,TSU 100084', maxsplit=1)
 print(splitStr)
 
-# re.sub() 用一个新的子串替换字符串中正则表达式匹配到的字符
+# re.sub(pattern ,repl,string,count,flags) 用一个新的子串替换字符串中正则表达式匹配到的字符
 
 str = 'BIU 100081,TSU 100084'
 newStr = re.sub(r'[1-9]\d{5}', ':zipcode', str)
